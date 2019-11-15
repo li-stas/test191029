8543b4d8a09206e9a85eb980ffaebd760884532c
lista <lista@bk.ru>
2019-11-15 10:52:27 +0200

    Сделан отделный пакет для тестов Коллекции

--- src/main/java/com/lista/TestCollect/TestCollect.java
+++ src/main/java/com/lista/TestCollect/TestCollect.java
@@ -5,14 +5,14 @@ import com.lista.Student;
 import java.util.*;
 
 public class TestCollect {
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
 
         long[][] aStatistics = {
-                {0,0,0},
-                {0,0,0},
-                {0,0,0},
-                {0,0,0},
-                {0,0,0}
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
         };
         int nCntAdd = 10000;
         Scanner scan = new Scanner(System.in);
@@ -24,32 +24,32 @@ public class TestCollect {
             switch (nChoice) { 
                 case 1: //System.out.println("1. ArrayList                     ");
                     ArrayList<Student>  dbArrayList = new ArrayList<>();
-                    TestCollectMake.mTestList(dbArrayList,nCntAdd,aStatistics[0]);
+                    TestCollectMake.mTestList(dbArrayList, nCntAdd, aStatistics[0]);
                     TestCollectShow.elem_aStatisticsShow(aStatistics[0]);
                     //System.out.println("" + Arrays.toString(aStatistics[0]));
                     break;
                 case 2: //System.out.println("2. LinkedList                    ");
                     LinkedList<Student>  dbLinkedList = new LinkedList<>();
-                    TestCollectMake.mTestList(dbLinkedList,nCntAdd,aStatistics[1]);
+                    TestCollectMake.mTestList(dbLinkedList, nCntAdd, aStatistics[1]);
                     TestCollectShow.elem_aStatisticsShow(aStatistics[1]);
                     break;
                 case 3: //System.out.println("3. HashSet                       ");
                     HashSet<Student>  dbHashSet = new HashSet<>();
-                    TestCollectMake.mTestSet(dbHashSet,nCntAdd,aStatistics[2]);
+                    TestCollectMake.mTestSet(dbHashSet, nCntAdd, aStatistics[2]);
                     TestCollectShow.elem_aStatisticsShow(aStatistics[2]);
                     break;
                 case 4: //System.out.println("4. HashMap                       ");
-                    HashMap<String,Student>  dbHashMap = new HashMap<>();
+                    HashMap<String, Student>  dbHashMap = new HashMap<>();
                     TestCollectMake.mTestMap(dbHashMap,  nCntAdd, aStatistics[3]);
                     TestCollectShow.elem_aStatisticsShow(aStatistics[3]);
                     break;
                 case 5: //System.out.println("5. TreeMap                       ");
-                    TreeMap<String,Student>  dbTreeMap = new TreeMap<>();
+                    TreeMap<String, Student>  dbTreeMap = new TreeMap<>();
                     TestCollectMake.mTestMap(dbTreeMap,  nCntAdd, aStatistics[4]);
                     TestCollectShow.elem_aStatisticsShow(aStatistics[4]);
                     break;
                 case 6: //System.out.println("6. Statistics                    ");
-                    TestCollectShow.all_aStatisticsShow( nCntAdd,aStatistics);
+                    TestCollectShow.all_aStatisticsShow(nCntAdd, aStatistics);
                     break;
                 case 0: //System.out.println("0. Exit                          ");
                     lExit = true;
--- src/main/java/com/lista/TestCollect/TestCollectMake.java
+++ src/main/java/com/lista/TestCollect/TestCollectMake.java
@@ -10,18 +10,18 @@ public class TestCollectMake {
     static void mTestSet(Set<Student> dbHashSet, int nCntAdd, long[] aStatistic) {
         Student getStudent;
         long startTime = System.currentTimeMillis();
-        for (int i = 1; i <= nCntAdd; i++  ){
-            dbHashSet.add(new Student("stud" + i,18, 1000 + i ));
+        for (int i = 1; i <= nCntAdd; i++) {
+            dbHashSet.add(new Student("stud" + i, 18, 1000 + i));
         }
         aStatistic[0] = System.currentTimeMillis() - startTime;
         startTime = System.currentTimeMillis();
-        for ( Student studXXX:dbHashSet  ) {
+        for (Student studXXX:dbHashSet) {
             getStudent = studXXX;
         }
 
         aStatistic[1] = System.currentTimeMillis() - startTime;
         startTime = System.currentTimeMillis();
-        for (int i = 1; i <= nCntAdd; i++  ){
+        for (int i = 1; i <= nCntAdd; i++) {
             dbHashSet.remove(0);
         }
         aStatistic[2] = System.currentTimeMillis() - startTime;
@@ -30,17 +30,17 @@ public class TestCollectMake {
      static void mTestMap(Map<String, Student> dbHashMap, int nCntAdd, long[] aStatistic) {
         Student getStudent;
         long startTime = System.currentTimeMillis();
-        for (int i = 1; i <= nCntAdd; i++  ){
-            dbHashMap.put( "stud" + i, new Student("stud" + i,18, 1000 + i ));
+        for (int i = 1; i <= nCntAdd; i++) {
+            dbHashMap.put("stud" + i, new Student("stud" + i, 18, 1000 + i));
         }
         aStatistic[0] = System.currentTimeMillis() - startTime;
         startTime = System.currentTimeMillis();
-        for(Map.Entry<String,Student> entry: dbHashMap.entrySet()) {
+        for (Map.Entry<String, Student> entry: dbHashMap.entrySet()) {
             getStudent = entry.getValue();
         }
         aStatistic[1] = System.currentTimeMillis() - startTime;
         startTime = System.currentTimeMillis();
-        for (int i = 1; i <= nCntAdd; i++  ){
+        for (int i = 1; i <= nCntAdd; i++) {
             dbHashMap.remove("stud" + i);
         }
         aStatistic[2] = System.currentTimeMillis() - startTime;
@@ -49,17 +49,17 @@ public class TestCollectMake {
     static void mTestList(List<Student> dbArrayList, int nCntAdd, long[] aStatistic) {
         Student getStudent;
         long startTime = System.currentTimeMillis();
-        for (int i = 1; i <= nCntAdd; i++  ){
-            dbArrayList.add(new Student("stud" + i,18, 1000 + i ));
+        for (int i = 1; i <= nCntAdd; i++) {
+            dbArrayList.add(new Student("stud" + i, 18, 1000 + i));
         }
         aStatistic[0] = System.currentTimeMillis() - startTime;
         startTime = System.currentTimeMillis();
-        for (int i = 1; i <= nCntAdd; i++  ){
+        for (int i = 1; i <= nCntAdd; i++) {
             getStudent = dbArrayList.get(i - 1);
         }
         aStatistic[1] = System.currentTimeMillis() - startTime;
         startTime = System.currentTimeMillis();
-        for (int i = 1; i <= nCntAdd; i++  ){
+        for (int i = 1; i <= nCntAdd; i++) {
             dbArrayList.remove(0);
         }
         aStatistic[2] = System.currentTimeMillis() - startTime;
